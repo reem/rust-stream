3685d6e88dfee7509c318f0d4b440761b1b0a9f3
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-02 16:43:04 -0700

    (feat) Inline all the things.

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,15 @@ pub struct Stream<T> {
 
 impl<T: Send + Share> Stream<T> {
     /// Get, and force evaluation of, the value at the front of the Stream.
+    #[inline]
     pub fn head(&self) -> &T { &*self.head }
 
     /// Get the rest of the stream, skipping the first value.
+    #[inline]
     pub fn tail(&self) -> Arc<Stream<T>> { self.tail.deref().clone() }
 
     /// Get an infinite iterator over the contents of the stream.
+    #[inline]
     pub fn iter<'a>(&'a self) -> StreamIter<'a, T> {
         StreamIter { stream: self }
     }
@@ -33,6 +36,7 @@ impl<T: Send + Share> Stream<T> {
 pub struct StreamIter<'a, T> { stream: &'a Stream<T> }
 
 impl<'a, T: Send + Share> Iterator<&'a T> for StreamIter<'a, T> {
+    #[inline]
     fn next(&mut self) -> Option<&'a T> {
         let result = self.stream.head();
         self.stream = &**self.stream.tail;
